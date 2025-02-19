890da889b9661858f87e72e02a12968d2b7d035a
mentipento <26530260+mentipento@users.noreply.github.com>
2025-02-19 17:44:31 +0100

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,16 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
+const { merge } = require('webpack-merge');
+const path = require('path');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  mode: "development",
-  devtool: "inline-source-map",
+  mode: 'development',
+  devtool: 'inline-source-map',
   devServer: {
-    static: "./dist",
+    hot: true,
+    open: true,
+    port: 8080,
+    historyApiFallback: true,
+    // Watch the template file for changes
+    watchFiles: [path.resolve(__dirname, 'src/template.html')],
   },
 });
